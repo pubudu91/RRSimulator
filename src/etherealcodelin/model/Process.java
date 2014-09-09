9bef9064b53b589e10b0d5329d90153c8d6308e4
Pubudu Fernando <pubudu.fernando@gmail.com>
2014-09-09 21:20:13 +0530

    Initial commit of the project

---
+++ src/etherealcodelin/model/Process.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package etherealcodelin.model;
+
+/**
+ *
+ * @author Pubudu
+ */
+public class Process {
+    private int PID;    // Process ID
+    private String name;    // Name of the process
+    private int duration;   // Total CPU time required for this process to finish execution
+}
