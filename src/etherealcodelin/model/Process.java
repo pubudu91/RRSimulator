24868a16503ef2da29e1b5fc405f2a28bd0270e2
Pubudu Fernando <pubudu.fernando@gmail.com>
2014-09-09 21:48:13 +0530

    Third commit: added getter methods to Process.java

--- src/etherealcodelin/model/Process.java
+++ src/etherealcodelin/model/Process.java
@@ -11,7 +11,29 @@ package etherealcodelin.model;
  * @author Pubudu
  */
 public class Process {
-    private int PID;    // Process ID
+    private int pid;    // Process ID
     private String name;    // Name of the process
     private int duration;   // Total CPU time required for this process to finish execution
+    
+    public Process(int pid, String name, int duration){
+        this.pid = pid;
+        this.name = name;
+        this.duration = duration;
+    }
+    
+    public int getPID(){
+        return pid;
+    }
+    
+    public String getName() {
+        return name;
+    }
+    
+    public int getDuration() {
+        return duration;
+    }
+    
+    public String toString(){
+        return "[ "+pid+": "+name+", "+duration+" ]";
+    }
 }
